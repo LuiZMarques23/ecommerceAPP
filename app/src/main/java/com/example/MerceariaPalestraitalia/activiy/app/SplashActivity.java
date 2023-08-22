176f5b508338c92a9497b32d8bf43e91cf12ce0b
luiz2 <ls360490@mail.com>
2023-08-22 19:20:16 -0300

    Projeto ecommerce

--- app/src/main/java/com/example/MerceariaPalestraitalia/activiy/app/SplashActivity.java
+++ app/src/main/java/com/example/MerceariaPalestraitalia/activiy/app/SplashActivity.java
@@ -24,7 +24,7 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        new Handler(getMainLooper()).postDelayed(this::verificaAcesso,2004);
+        new Handler(getMainLooper()).postDelayed(this::verificaAcesso,3000);
 
        recuperaDados();
 
